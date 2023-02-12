0fbc8de9fd6c052a8833b0c9d29c701b37db3a66
dieter <pankratzdieter@gmail.com>
2023-02-12 10:39:02 +0100

    Implement logic to establish connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use http::Method;
+use http::Request;
+use server::Server;
+
+mod http;
+mod server;
+
+fn main() { 
+    let server = Server::new("127.0.0.1:8080".to_string());
+     server.run();
 }
+
+
---
+++ src/server.rs
@@ -0,0 +1,31 @@
+    use std::net::TcpListener;
+pub struct Server {
+        addr: String,
+    }
+
+    impl Server {
+       pub fn new(addr: String) -> Self {
+            Self {
+                addr
+            }
+        }
+
+        pub fn run(self) {
+            println!("Listening on {}", self.addr);
+            
+            let listener = TcpListener::bind(&self.addr).unwrap();
+
+            loop {
+                match listener.accept() {
+                    Ok((stream, _)) => {
+                        let a = 5;
+                        println!("Ok");
+                    },
+                    Err(e) => println!("Failed to establish a connection: {}", e),    
+                }
+            }
+        }
+
+    }
+
+
